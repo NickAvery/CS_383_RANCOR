3866811ea7f7f5e526f4dd0c5d3479db0b94800a
NickAvery <35745096+NickAvery@users.noreply.github.com>
2018-02-21 14:31:35 -0800

    Fixes to enemy.h
    changed x, y-coordinates to qreal
    added QGraphicsRectItem capabilities

--- inc/Nick/enemy.h
+++ inc/Nick/enemy.h
@@ -1,22 +1,25 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
-
-class Enemy
+#include <QGraphicsRectItem>
+#include <QGraphicsScene>
+#include <QDebug>
+class Enemy: public QGraphicsRectItem
 {
 private:
-    double mXPos;
-    double mYPos;
+    qreal mXPos;
+    qreal mYPos;
     int mHealth;
     int mAtkValue;
     int mSize;
 public:
     Enemy(double x, double y, int size); //x = x-coordinate, y = y-coordinate, size = size of square
-    double getXPos() {return mXPos;} //gets the x-coordinate
-    double getYPos() {return mYPos;} //gets the y-coordinate
+    double getXPos() {return x();} //gets the x-coordinate
+    double getYPos() {return y();} //gets the y-coordinate
     int getHealth() {return mHealth;} //gets the health value
     int getAtkValue() {return mAtkValue;} //gets the attack value
-    void move(double x, double y){mXPos = x; mYPos = y;} //sets the position to the (x, y) coordinate
+    int getSize() {return mSize;}
+    void move(qreal x, qreal y){setPos(x, y);} //sets the position to the (x, y) coordinate
     int attack();
 };
 
