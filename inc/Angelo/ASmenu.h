52418b67645630ac8d6cf0e31c15be0ddde4d1fe
Evangelos Stratigakes <angelostratigakes@gmail.com>
2018-02-21 17:14:02 -0800

    Update ASmenu.h

--- inc/Angelo/ASmenu.h
+++ inc/Angelo/ASmenu.h
@@ -1,1 +1,30 @@
 
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+#include <QMainWindow>
+
+class MainMenu;
+
+namespace Ui{
+	class MainMenu;
+}
+
+class MainMenu : public MainMenu
+{
+	Q_OBJECT
+}
+
+public:
+	explicit MainMenu(QWidget *parent = 0);
+	~MainMenu();
+
+private slots:
+	void on_pushButton_clicked(Game game);
+
+private:
+	UI::MainMenu *ui;
+
+};
+
+#endif
