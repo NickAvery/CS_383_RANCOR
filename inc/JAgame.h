ccdedfc15cf46e0bcc979227b7b87a46d496d79f
JacobAlderink <35742056+JacobAlderink@users.noreply.github.com>
2018-02-21 17:46:26 -0800

    Add files via upload

--- inc/JAgame.h
+++ inc/JAgame.h
@@ -1,5 +1,8 @@
 #include <QGraphicsView>
 #include <QGraphicsScene>
+#include <QTimer>
+#include "NAenemyupdater.h"
+//#include "BTcharacter.h"
 
 
 class Game : public QGraphicsView{
@@ -8,22 +11,21 @@ class Game : public QGraphicsView{
  private:
   QGraphicsScene* scene;
   //the scene on which the enemies and the map will be displayed.
-  
+  //Character* Player;
+  QTimer * timer;
+  Enemy **enemies;
+  EnemyUpdater* eUpdater;
  public:
   Game(QWidget* parent = NULL);
-  //Initializes a new game
-  
-
-  QGraphics* getScene();
-  //returns the scene that was initialized in the constructed in the constructor that them enemies and map objects were created and displayed on
-  
 
 
   void mainMenu();
-
+    //Initializes a new game
   void start(int CharClass);
     //int CharClass represents the class(warrior, ranger, DR.BC) that the user choose
     //this will be called by UI once the player chooses their class and presses start game
-     
+
+public slots:
+  void levelLoop();
 };
   
