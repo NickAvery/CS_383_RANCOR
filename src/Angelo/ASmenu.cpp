402c39ddad62fcd9b3e08bd11cd00495d7f403b5
Evangelos Stratigakes <angelostratigakes@gmail.com>
2018-02-21 17:11:49 -0800

    Update ASmenu.cpp

--- src/Angelo/ASmenu.cpp
+++ src/Angelo/ASmenu.cpp
@@ -1,1 +1,22 @@
 
+#include "asmenu.h"
+#include "ui_asmenu.h"
+#include <QGraphicsScene>
+#include <JAgame.h>
+
+MainMenu:: MainMenu(QWidget *parent)	:
+	QMainWindow(parent), ui(new Ui::MainWindow)
+	{
+		ui->setupUI(this);
+	}
+
+MainMenu::~MainMenu()
+{
+	delete ui;
+}
+
+void MainMenu::on_pushButton_clicked(Game)
+{
+	Game game;
+	game->start();
+}
