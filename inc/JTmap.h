ea4c7b1e5753dfee31ce9a58152e6c5ca49e11f4
Sentoushi <35748461+Sentoushi@users.noreply.github.com>
2018-03-03 14:09:20 -0800

    Add files via upload

--- inc/JTmap.h
+++ inc/JTmap.h
@@ -2,6 +2,7 @@
 #define JTMAP_H
 
 #include "JTwalls.h"
+#include "JTroom.h"
 #include <QGraphicsView>
 #include <QWidget>
 #include <QGraphicsScene>
@@ -12,6 +13,11 @@ public:
     QGraphicsScene * scene;
     Walls * walls;
     Map(QGraphicsScene* scene);
+    Room * selectRoom(int selection, QGraphicsScene* scene);
+    static int roomx;
+    static int roomy;
+    static int maxx;
+    static int maxy;
 };
 
 #endif // JTMAP_H
