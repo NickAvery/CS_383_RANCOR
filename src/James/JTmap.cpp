5907692e63b6651c40b2cb1f079a788f0ccd360a
Sentoushi <35748461+Sentoushi@users.noreply.github.com>
2018-03-03 16:10:06 -0800

    Add files via upload
    Avoiding Segfault in map section

--- src/James/JTmap.cpp
+++ src/James/JTmap.cpp
@@ -34,13 +34,14 @@ Map::Map(QGraphicsScene* scene, bool Demo){
 
     //qDebug() << coords;
     Room * floorarray[maxy][maxx];
-    Room * room1 = selectRoom(1, scene);
+   //room = selectRoom(1, scene);
+    selectRoom(1,scene);
 
     //later, switch this out for real map creation
     //make sure to always add to the solution string
     for(int i=0; i<maxy; i++){
         for(int j=0; j<maxx; j++){
-            floorarray[i][j]=room1;
+            floorarray[i][j]=room;
         }
     }
 
@@ -52,59 +53,59 @@ Map::Map(QGraphicsScene* scene, bool Demo){
     }
 }
 
-Room * Map::selectRoom(int selection, QGraphicsScene* scene){
-    Room * room1;
+void Map::selectRoom(int selection, QGraphicsScene* scene){
+    //Room * room1;
     switch(selection){
     case 1:
-        room1 = new Room(scene, true, true, true, true);         //this will be attached to the 4 door case
+        room = new Room(scene, true, true, true, true);         //this will be attached to the 4 door case
         break;
     case 2:
-        room1 = new Room(scene, true, false, false, false);         //this will be attached to the top door case
+        room = new Room(scene, true, false, false, false);         //this will be attached to the top door case
         break;
     case 3:
-        room1 = new Room(scene, false, true, false, false);         //this will be attached to the right door case
+        room = new Room(scene, false, true, false, false);         //this will be attached to the right door case
         break;
     case 4:
-        room1 = new Room(scene, false, false, true, false);         //this will be attached to the bottom door case
+        room = new Room(scene, false, false, true, false);         //this will be attached to the bottom door case
         break;
     case 5:
-        room1 = new Room(scene, false, false, false, true);         //this will be attached to the left door case
+        room = new Room(scene, false, false, false, true);         //this will be attached to the left door case
         break;
     case 6:
-        room1 = new Room(scene, true, true, false, false);         //this will be attached to the top && right door case
+        room = new Room(scene, true, true, false, false);         //this will be attached to the top && right door case
         break;
     case 7:
-        room1 = new Room(scene, true, false, true, false);         //this will be attached to the 2 vertical door case
+        room = new Room(scene, true, false, true, false);         //this will be attached to the 2 vertical door case
         break;
     case 8:
-        room1 = new Room(scene, true, false, true, false);         //this will be attached to the top && left door case
+        room = new Room(scene, true, false, true, false);         //this will be attached to the top && left door case
         break;
     case 9:
-        room1 = new Room(scene, false, true, true, false);         //this will be attached to the right && bottom door case
+        room = new Room(scene, false, true, true, false);         //this will be attached to the right && bottom door case
         break;
     case 10:
-        room1 = new Room(scene, false, true, false, true);         //this will be attached to the 2 horizontal door case
+        room = new Room(scene, false, true, false, true);         //this will be attached to the 2 horizontal door case
         break;
     case 11:
-        room1 = new Room(scene, false, false, true, true);         //this will be attached to the bottom && left door case
+        room = new Room(scene, false, false, true, true);         //this will be attached to the bottom && left door case
         break;
     case 12:
-        room1 = new Room(scene, false, true, true, true);         //this will be attached to the not-top door case
+        room = new Room(scene, false, true, true, true);         //this will be attached to the not-top door case
         break;
     case 13:
-        room1 = new Room(scene, true, false, true, true);         //this will be attached to the not-right door case
+        room = new Room(scene, true, false, true, true);         //this will be attached to the not-right door case
         break;
     case 14:
-        room1 = new Room(scene, true, true, false, true);         //this will be attached to the not-bottom door case
+        room = new Room(scene, true, true, false, true);         //this will be attached to the not-bottom door case
         break;
     case 15:
-        room1 = new Room(scene, true, true, true, false);         //this will be attached to the not-left door case
+        room = new Room(scene, true, true, true, false);         //this will be attached to the not-left door case
         break;
     case 16:
     default:
-        room1 = new Room(scene, false, false, false, false);        //handles problem case. 0 doors.
+        room = new Room(scene, false, false, false, false);        //handles problem case. 0 doors.
         break;
 
     }
-    return room1;
+    //return room1;
 }
