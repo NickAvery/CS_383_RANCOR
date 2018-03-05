f29e7d9b0b7521ced86dfedb262e7c33e4f22415
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2018-03-05 12:54:54 -0800

    Update .cpp files

--- src/Karstin/KNSkillManager.cpp
+++ src/Karstin/KNSkillManager.cpp
@@ -1,27 +1,37 @@
 #include "KNSkillManager.h"
+#include <iostream>
 
 SkillManager::SkillManager(Character *parent, int classID) {
-  parent->setSpeed(2);
+  DB = new DataBank();
+  DB->size = 40;
+  totalLevel = 1;
   if (this->selectClassType(classID) == 0) {
     //Incorrect class type selection
+    return NULL;
   }
+  //Give parent a copy of the data struct
+  parent->setData(DB);
 }
 
 SkillManager::~SkillManager() {
   
 }
 
+//Sets base defaults for skills based on given classID specifications
+//Returns 0 if invalid classID, returns 1 if valid classID
 int SkillManager::selectClassType(int classID) {
-  switch(classID) {
-  case(Skill::WARRIOR):
-    break;
-  case(Skill::RANGER):
-    break;
-  case(Skill::DR_BC):
-    break;
-  default:
-    break;
-  };
+  ClassType *ct = classTypeDatabase->getClassType(classID);
+  if (ct != NULL) {
+    healthSkill    = new Skill(Skill::HEALTH,    ct->getSkillLevel(Skill::HEALTH));
+    speedSkill     = new Skill(Skill::SPEED,     ct->getSkillLevel(Skill::SPEED));
+    damageSkill    = new Skill(Skill::DAMAGE,    ct->getSkillLevel(Skill::DAMAGE));
+    fireRateSkill  = new Skill(Skill::FIRERATE,  ct->getSkillLevel(Skill::FIRERATE));
+    shotSpeedSkill = new Skill(Skill::SHOTSPEED, ct->getSkillLevel(Skill::SHOTSPEED));
+    this->updateDataBank();
+    DB->totalHealth = healthSkill->getLevel();
+    return 1;
+  }
+  return 0;
 }
 
 int SkillManager::getSelectedClassTypeID() {
@@ -29,11 +39,24 @@ int SkillManager::getSelectedClassTypeID() {
 }
 
 int SkillManager::getSkillLevel(int skillID) {
-  
+  switch(skillID) {
+  case(Skill::HEALTH):
+    return healthSkill->getLevel();
+  case(Skill::SPEED):
+    return speedSkill->getLevel();
+  case(Skill::DAMAGE):
+    return damageSkill->getLevel();
+  case(Skill::FIRERATE):
+    return fireRateSkill->getLevel();
+  case(Skill::SHOTSPEED):
+    return shotSpeedSkill->getLevel();
+  default:
+    return -1;
+  };  
 }
 
 int SkillManager::getTotalLevel() {
-  
+  return totalLevel;
 }
 
 void SkillManager::addExperience(int amount) {
@@ -45,5 +68,13 @@ void SkillManager::applyPowerUp(PowerUp *p) {
 }
 
 void SkillManager::tickDownPowerUps(double seconds) {
-  
+
+}
+
+void SkillManager::updateDataBank() {
+  DB->currentHealth = healthSkill->getLevel();
+  DB->speed = speedSkill->getLevel();
+  DB->damage = damageSkill->getLevel();
+  DB->fireRate = fireRateSkill->getLevel();
+  DB->shotSpeed = shotSpeedSkill->getLevel();
 }
