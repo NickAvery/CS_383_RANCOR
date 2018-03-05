33ebd4ef6d7deca03690a5b95522795785d22c63
Evangelos Stratigakes <angelostratigakes@gmail.com>
2018-03-05 02:07:42 -0800

    Update ASVictoryScreen.cpp

--- src/Angelo/ASVictoryScreen.cpp
+++ src/Angelo/ASVictoryScreen.cpp
@@ -1,1 +1,32 @@
+#include "victoryscreen.h"
+#include "ui_victoryscreen.h"
+
+VictoryScreen::VictoryScreen(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::VictoryScreen)
+{
+    ui->setupUi(this);
+}
+
+VictoryScreen::~VictoryScreen()
+{
+    delete ui;
+}
+
+void VictoryScreen::on_pushButton_clicked() //return
+{
+
+}
+
+void VictoryScreen::on_pushButton_2_clicked() //exit
+{
+    exit( EXIT_SUCCESS );
+}
+
+void VictoryScreen::display()
+{
+    VictoryScreen screen;
+    screen.setModal(true);
+    screen.exec();
+}
 
