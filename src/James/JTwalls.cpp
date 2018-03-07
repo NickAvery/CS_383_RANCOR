83402f199aa91f4065573f1c6d3191d6f1cdfe4b
Baylus Tunnicliff <irishtunn@gmail.com>
2018-03-07 09:34:04 -0800

    Revert "Add files via upload"
    This reverts commit df7a65ccd7bd8810cc040110082754b64554f9cb.

--- src/James/JTwalls.cpp
+++ src/James/JTwalls.cpp
@@ -1,6 +1,6 @@
 #include "JTwalls.h"
 
-Walls::Walls(): QObject(), QGraphicsPixmapItem(){
-    setPixmap(QPixmap(":/images/Room.ai"));
+Walls::Walls(): QObject(), QGraphicsRectItem(){
+    setRect(0,0,720,520);
     setPos(40,40);
 }
