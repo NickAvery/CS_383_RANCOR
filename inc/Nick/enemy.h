49cac73a14b0a8fe649b52d7a5c3ff6e2e76c750
Nick Avery <35745096+NickAvery@users.noreply.github.com>
2018-04-02 14:29:08 -0700

    Adding changes to headers for most recent changes

--- inc/Nick/enemy.h
+++ inc/Nick/enemy.h
@@ -6,6 +6,8 @@
 #include <QDebug>
 #include <QGraphicsPixmapItem>
 #include <QtMath>
+#include <cassert>
+#include <QUrl>
 class Enemy: public QGraphicsPixmapItem
 {
     //QGraphicsPixmapItem holds things such as position and the image.
@@ -14,15 +16,15 @@ private:
     int mAtkValue;
     int mSize;
 public:
-    Enemy(double x, double y, int size);    //x = x-coordinate, y = y-coordinate, size = size of square
+    Enemy(double, double, int, QPixmap);    //x = x-coordinate, y = y-coordinate, size = size of square
     qreal getXPos();                        //gets the x-coordinate
     qreal getYPos();                        //gets the y-coordinate
     int getHealth();                        //gets the health value
     int getAtkValue();                      //gets the attack value
     int getSize();                          //get the size of an enemy
-    void move(qreal, qreal);            //sets the position to the (x, y) coordinate
+    void move(qreal, qreal);                //sets the position to the (x, y) coordinate
     int attack();                           //enemy deals damage
-    void getAttacked(int);           //enemy is hit
+    void attacked(int);                   //enemy is hit
     void removeEnemy();                     //if an enemy dies or gets removed for another reason, use this
 };
 
--- inc/Nick/enemyspawner.h
+++ inc/Nick/enemyspawner.h
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <QFile>
 class EnemySpawner
 {
 private:
