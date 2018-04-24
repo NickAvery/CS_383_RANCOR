58d434602026abf65255a246165f7dc87d9e5847
Nick Avery <35745096+NickAvery@users.noreply.github.com>
2018-04-23 23:22:16 -0700

    Add files via upload

--- inc/Nick/rangedenemy.h
+++ inc/Nick/rangedenemy.h
@@ -4,11 +4,20 @@
 
 #include "enemy.h"
 
-class RangedEnemy : public Enemy {
+class RangedEnemy : public QObject, public Enemy {
+
+    Q_OBJECT
 public:
     RangedEnemy(double, double, int);
+    ~RangedEnemy();
     void makeAttack(qreal, qreal);
     int decide(qreal, qreal);
+private:
+    int shotCooldown;
+    int tempCooldown;
+signals:
+    void shotTick();
+    void shotKill();
 };
 
 #endif // RANGEDENEMY_H
